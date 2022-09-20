8a6eef4532c14a450c15ada3b2948bc1372cebe6
mawathilde <mawathilde@gmail.com>
2022-09-20 13:12:33 +0200

    feat: add background & sounds

--- scenes/Main.gd
+++ scenes/Main.gd
@@ -10,6 +10,8 @@ func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
+	$Music.stop();
+	$DeathSound.play();
 
 func new_game():
 	score = 0
@@ -18,6 +20,7 @@ func new_game():
 	get_tree().call_group("mobs", "queue_free")
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$Music.play()
 
 func _on_ScoreTimer_timeout():
 	score += 1
